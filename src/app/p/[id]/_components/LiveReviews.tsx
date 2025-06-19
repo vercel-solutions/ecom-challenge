5c5b2eb182584d6af1e6cb0be01b20ef42377f98
sergiolemus <sergio.lemus.mail@gmail.com>
2025-06-19 01:15:07 -0400

    C5: Update PDP for 1s live updates on review count for products with price > 50

---
+++ src/app/p/[id]/_components/LiveReviews.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import api from "@/api";
+import { useEffect, useState } from "react";
+
+export const LiveReviews = ({ id }: { id: string }) => {
+  const [reviews, setReviews] = useState<number | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const product = await api.product.get(id);
+      console.debug("Reviews Live Update!");
+      setReviews(product.reviews);
+    };
+
+    fetchData();
+    const interval = setInterval(fetchData, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <span className="ml-2 text-sm">({reviews} reviews)</span>;
+};
--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -5,6 +5,7 @@ import {Button} from "@/components/ui/button";
 import api from "@/api";
 import { Suspense } from "react";
 import { RelatedProducts } from "./_components/RelatedProducts";
+import { LiveReviews } from "./_components/LiveReviews";
 
 export async function generateStaticParams() {
   const products = await api.product.list();
@@ -16,6 +17,8 @@ export async function generateStaticParams() {
 export default async function ProductDetailPage({params}: {params: Promise<{id: string}>}) {
   const {id} = await params;
   const product = await api.product.get(id);
+
+  const liveUpdateReviews = Boolean(product.price > 50);
 
   return (
     <div className="container flex-1 px-4 py-4 md:px-6 md:py-12">
@@ -47,7 +50,13 @@ export default async function ProductDetailPage({params}: {params: Promise<{id: 
                   />
                 ))}
               </div>
-              <span className="ml-2 text-sm">({product.reviews} reviews)</span>
+              {liveUpdateReviews ? (
+                <LiveReviews id={id} />
+              ) : (
+                <span className="ml-2 text-sm">
+                  ({product.reviews} reviews)
+                </span>
+              )}
             </div>
             <p className="mb-4 text-2xl font-bold">
               ${product.price.toFixed(2)}
@@ -75,7 +84,7 @@ export default async function ProductDetailPage({params}: {params: Promise<{id: 
         <h2 className="mb-6 text-2xl font-bold">Related Products</h2>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           <Suspense fallback={<div>Loading...</div>}>
-            <RelatedProducts product={product}/>
+            <RelatedProducts product={product} />
           </Suspense>
         </div>
       </div>
